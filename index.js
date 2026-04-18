1ce06534d66c11b95ad2df8b3755bdfcf108cc3c
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-04-18 17:52:31 +0300

    made first exersice but not as a map

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log ("Exercise 1: Uppercase Names")
-console.log("- - - - - - - - - - - - - -")
+console.log ("Exercise 1: Uppercase Names");
+console.log("- - - - - - - - - - - - - -");
+
+let constusers= [
+  { name:"alex" },
+  { name:"maria" },
+  { name:"john" }
+];
+
+
+
+
+// for(let i = 0; i <constusers.length;i++){
+// constusers[i].name=constusers[i].name.toUpperCase();
+// console.log(constusers[i]);
+// }
+const mapConstusers= new Map(constusers.map((o)=>[o.name,o]));
+console.log(mapConstusers);
+//https://www.geeksforgeeks.org/javascript/how-to-convert-an-array-of-objects-to-a-map-in-javascript/
