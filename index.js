380717e3c77424efa30ef9a9b1145302e363a195
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-04-20 15:53:02 +0300

    exerscise 2

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ let constusers= [
 
 
 
-// for(let i = 0; i <constusers.length;i++){
-// constusers[i].name=constusers[i].name.toUpperCase();
-// console.log(constusers[i]);
-// }
-const mapConstusers= new Map(constusers.map((o)=>[o.name,o]));
-console.log(mapConstusers);
+for(let i = 0; i <constusers.length;i++){
+constusers[i].name=constusers[i].name.toUpperCase();
+console.log(constusers[i]);
+ }
+// const mapConstusers= new Map(constusers.map((o)=>[o.name,o]));
+// console.log(mapConstusers);
 //https://www.geeksforgeeks.org/javascript/how-to-convert-an-array-of-objects-to-a-map-in-javascript/
+
+
+
+console.log ("Exercise 2: Add Greeting");
+console.log("- - - - - - - - - - - - - -");
+
+
+for (let i = 0;i<constusers.length; i++){
+console.log("hello  " + constusers[i].name);
+
+}
+
